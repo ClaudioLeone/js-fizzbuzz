88a32d69bd2e0697346ec59d923d8028b495d4b9
ClaudioLeone <claleone92@gmail.com>
2023-03-10 18:51:29 +0100

    [BONUS 2] Completed boxes stylization, implemented bg color randomization for not 'Fuzz', not 'Buzz' and not 'FuzzBuzz' boxes

--- JS/script.js
+++ JS/script.js
@@ -1,21 +1,37 @@
-const grid = document.querySelector(".grid")
+const grid = document.querySelector(".grid");
+const box = document.querySelector(".box");
+const background = document.getElementById("background");
+
 for (let i = 1; i <= 100; i++) {
     let numType;
+    let color;
+
+    // COLOR RANDOMIZER VARIABLES
+    let r = Math.floor(Math.random() * 256);
+    let g = Math.floor(Math.random() * 256);
+    let b = Math.floor(Math.random() * 256);
+    let rgb = "rgb(" + r + "," + g + "," + b + ")";
+
     if (i % 3 === 0 && i % 5 === 0){
         numType = "FizzBuzz";
-        console.log(i, numType); 
+        console.log(i, numType);
+        color = "fizzbuzz-color"
+        grid.innerHTML += `<div class="box ${color}"><span class="box-content">${numType}</span></div>`;
     }
     else if(i % 3 === 0){
         numType = "Fizz";
         console.log(i, numType); 
+        color = "fizz-color"
+        grid.innerHTML += `<div class="box ${color}"><span class="box-content">${numType}</span></div>`;
     }
     else if(i % 5 === 0){
         numType = "Buzz";
-        console.log(i, numType); 
+        console.log(i, numType);
+        color = "buzz-color"
+        grid.innerHTML += `<div class="box ${color}"><span class="box-content">${numType}</span></div>`;
     }
-    else {
-       console.log(i); 
-    }    
-
-    grid.innerHTML += `<div class="box">${i}</div>`
-} 
+    else { 
+        grid.innerHTML += `<div style="background-color: ${rgb}" class="box ${color}"><span class="box-content">${i}</span></div>`;
+        console.log(i);
+    }
+}
