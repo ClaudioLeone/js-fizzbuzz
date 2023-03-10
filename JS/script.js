fc4108cfc94c9769e10ea38b55496d15e371729e
ClaudioLeone <claleone92@gmail.com>
2023-03-10 15:48:59 +0100

    Solved FizzBuzz problem [console], added 'grid' to html file

---
+++ JS/script.js
@@ -0,0 +1,19 @@
+const grid = document.querySelector(".grid")
+for (let i = 1; i <= 100; i++) {
+    let numType;
+    if (i % 3 === 0 && i % 5 === 0){
+        numType = "FizzBuzz";
+        console.log(i, numType); 
+    }
+    else if(i % 3 === 0){
+        numType = "Fizz";
+        console.log(i, numType); 
+    }
+    else if(i % 5 === 0){
+        numType = "Buzz";
+        console.log(i, numType); 
+    }
+    else {
+       console.log(i); 
+    }    
+}
